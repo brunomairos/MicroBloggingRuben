686acd0b8ad404b3d75f4f71b1c2b72aca4a32ee
Bruno Mairos <bferreira@DEVSCOPE125.devscope.local>
2017-01-13 14:57:36 +0000

    Class Utilizador Class Post

--- src/microbloggingruben/Utilizador.java
+++ src/microbloggingruben/Utilizador.java
@@ -10,5 +10,45 @@ package microbloggingruben;
  * @author rcunha
  */
 public class Utilizador {
+    private static int nUtilizadores;
+    private String nome;
+    private String nickname;
+    private String password;
+    //private (lista ligada) utSeguir;
+    //private (lista de posts) posts;
     
+    Utilizador(String nome, String nickname, String password){
+        setNome(nome);
+        setNickname(nickname);
+        setPassword(password);
+        nUtilizadores++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public int getNutilizadores() {
+        return nUtilizadores;
+    }
 }
